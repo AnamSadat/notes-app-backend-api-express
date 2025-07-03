731be0c6aa03bb284c43c7709befac3f8381e64d
AnamSadat <anamsadat3@gmail.com>
2025-07-03 20:06:22 +0700

    change prisma accelerate to postgres local

--- src/queries.ts
+++ src/queries.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient()
-  .$extends(withAccelerate());
 
 // A `main` function so that we can use async/await
 async function main() {
